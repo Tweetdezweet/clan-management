a594412ee21f523dd6bfe39f9a0c17c85d14ede5
Koen Gabriels <koen.gabriels@gmail.com>
2014-08-15 13:18:13 +0200

    Adds main plugin filie

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Clan Management
+ * Plugin URI: http://charliefoxtrot.org
+ * Description: A plugin to manage gaming communities
+ * Version: 0.1
+ * Author: Tweetdezweet
+ * Author URI: http://www.koengabriels.be
+ * License: GPL2
+ */
